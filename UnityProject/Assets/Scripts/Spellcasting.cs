57a0b35b7e2ae0ce386218861e50021c944e4aba
Łukasz Tenerowicz <lukasz.tenerowicz@itsilesia.com>
2014-02-28 22:31:51 +0100

    Added dummies for Melee attack and spellcasting

---
+++ UnityProject/Assets/Scripts/Spellcasting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spellcasting : MonoBehaviour {
+
+	public bool isSpellcasting = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( isSpellcasting ) {
+
+			if( Input.GetKey(KeyCode.Mouse0) ) {
+				Debug.Log("Attacking");
+			}
+		}	
+	}
+}
