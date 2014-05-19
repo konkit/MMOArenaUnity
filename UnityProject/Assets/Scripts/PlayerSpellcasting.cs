863877894d66f63981531eddfe94f74a026f6432
Łukasz Tenerowicz <lukasz.tenerowicz@itsilesia.com>
2014-05-19 13:41:00 +0200

    Added frostball and thunderball and switching between them.

--- UnityProject/Assets/Scripts/PlayerSpellcasting.cs
+++ UnityProject/Assets/Scripts/PlayerSpellcasting.cs
@@ -3,14 +3,27 @@ using System.Collections;
 
 public class PlayerSpellcasting : MonoBehaviour {
 	public GameObject fireballPrefab;
+	public GameObject frostballPrefab;
+	public GameObject thunderballPrefab;
+	GameObject cntPrefab = fireballPrefab;
+	int currentSpell = 1;
+	int maxSpells = 3;
 
 	public float spellHeight = 1.4f;
 
 	public float cooldownAmount = 1.0f, currentCooldown = 0.0f;
 
+
+
+	Rect currentSpellRect = new Rect(10, 10, 200, 30);
+
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void OnGUI() {
+		GUI.Label( currentSpellRect, "Current spell : " + GetSpellName( currentSpell ) );
 	}
 	
 	// Update is called once per frame
@@ -20,15 +33,80 @@ public class PlayerSpellcasting : MonoBehaviour {
 		}
 
 		if( Input.GetKey(KeyCode.Mouse0) ) {
-
 			if( currentCooldown <= 0.0f ) {
 				GetComponent<MouseController>().AlignRotation();
 
 				Debug.Log("Attacking");
-				Instantiate(fireballPrefab, (transform.position + new Vector3(0.0f, spellHeight, 0.0f)) + transform.forward, transform.rotation);
+
+
+
+				Instantiate(cntPrefab, (transform.position + new Vector3(0.0f, spellHeight, 0.0f)) + transform.forward, transform.rotation);
+
 
 				currentCooldown = cooldownAmount;
 			}
-		}	
+		} else if( Input.GetKeyDown( KeyCode.Q ) ) {
+			PreviousSpell();
+		} else if( Input.GetKeyDown( KeyCode.E ) ) {
+			NextSpell();
+		}
+	}
+
+	void PreviousSpell ()
+	{
+		if( currentSpell <= 1 ) {
+			currentSpell = maxSpells;
+		} else {
+			currentSpell--;
+		}
+
+		switch(currentSpell) {
+		case 1:
+			cntPrefab = fireballPrefab;
+			break;
+		case 2:
+			cntPrefab = frostballPrefab;
+			break;
+		case 3:
+			cntPrefab = thunderballPrefab;
+			break;
+		}
+	}
+	
+	void NextSpell ()
+	{
+		if( currentSpell >= maxSpells ) {
+			currentSpell = 1;
+		} else {
+			currentSpell++;
+		}
+
+		switch(currentSpell) {
+		case 1:
+			cntPrefab = fireballPrefab;
+			break;
+		case 2:
+			cntPrefab = frostballPrefab;
+			break;
+		case 3:
+			cntPrefab = thunderballPrefab;
+			break;
+		}
+	}
+	
+	string GetSpellName(int spellNum) {
+		switch(spellNum) {
+			case 1:
+				return "Fireball";
+				break;
+			case 2:
+				return "Frostball";
+				break;
+			case 3:
+				return "Thunderball";
+				break;
+		}
+
+		return "";
 	}
 }
