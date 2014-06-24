2e524b8788a449d22f83fe8d6c5682165187f984
Paweł Kluba <oskpkluba@gmail.com>
2014-06-24 23:56:31 +0200

    adding cube and explosions

---
+++ UnityProject/Assets/Scripts/CubeExplosion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeExplosion : MonoBehaviour {
+
+    public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "spell")
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
