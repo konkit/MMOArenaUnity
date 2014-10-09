a297149b99dbc4c56b8536db73eb9f5985c6bfe9
Łukasz Tenerowicz <lukasz.tenerowicz@itsilesia.com>
2014-10-09 19:57:20 +0200

    Added game version working on photon

---
+++ UnityProject/Assets/Scripts/Photon/KonkitNetworkView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KonkitNetworkView : MonoBehaviour {
+
+    private Vector3 correctPlayerPos;
+    private Quaternion correctPlayerRot;
+
+    public PhotonView photonView;
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.isMine)
+        {
+            transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+
+        }
+        else
+        {
+            // Network player, receive data
+            this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+            this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Photon/KonkitRandomMatchmaker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KonkitRandomMatchmaker : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby()
+    {
+        //PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRoom("asdf");
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        GameObject monster = PhotonNetwork.Instantiate("MAX_Photon", Vector3.zero, Quaternion.identity, 0);
+        monster.GetComponent<CharacterControlInterface>().enabled = true;
+        monster.GetComponent<PlayerMovementController>().enabled = true;
+        monster.GetComponent<MouseController>().enabled = true;
+        monster.GetComponent<HumanPlayerController>().enabled = true;
+    }
+}
